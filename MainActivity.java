387ec296ae46b302b04c662e714315dd0967d0e0
bluegang <452291963@qq.com>
2020-01-16 17:40:30 +0800

    php file

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+package noclay.im.zxingdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import noclay.im.zxing.activity.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivityForResult(new Intent(this, CaptureActivity.class), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) { //RESULT_OK = -1
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            Toast.makeText(MainActivity.this, scanResult, Toast.LENGTH_LONG).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
